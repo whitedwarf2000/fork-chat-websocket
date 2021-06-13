714cc99b6652fb32888f79e019bc26e739c5e998
Son Le <sontdt69@gmail.com>
2021-06-13 15:08:11 +0700

    Migrate from real time chat app

---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const NEW_CHAT_MESSAGE_EVENT = 'newChatMessageEvent';
+
+io.on('connection', socket => {
+  // eslint-disable-next-line no-console
+  console.log(`Client ${socket.id} connected!`);
+
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  socket.on(NEW_CHAT_MESSAGE_EVENT, data => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  socket.on('disconnect', () => {
+    // eslint-disable-next-line no-console
+    console.log(`Client ${socket.id} disconnected!`);
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${PORT}`);
+});
